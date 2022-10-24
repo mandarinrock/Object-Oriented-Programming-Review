53e932c2a84f53138d57feac431cfa88cbe7322e
mandarinrock <62828715+mandarinrock@users.noreply.github.com>
2022-10-24 00:56:00 -0700

    Problem 4 done

--- problem4.cpp
+++ problem4.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 using namespace std;
 
+// Global variables
 int x, y;
 int *px, *py;
 
 int f() {
+
+    // S is the sum of the values of x and y
+    // Static means that after the first time this function is called, the value of S will not change
     static int s = *px + *py;
+
+    // Print the value of S
     cout << s << endl;
+
+    // Return the value of S
     return s;
 }
 
 int main() {
+
+    // Assign value 2 to x and y
     x = y = 2;
+
+    // Assign address of x to px and address of y to py
     px = &x; py = &y;
+
+    // Assign value of f() to x and y
     x = y = f();
+
+    // Call f() again
     cout << f() << endl;
 }
