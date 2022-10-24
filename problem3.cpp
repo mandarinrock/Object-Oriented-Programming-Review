4c82247d1c7806113416e2579f7512e0753cb149
mandarinrock <62828715+mandarinrock@users.noreply.github.com>
2022-10-24 00:54:13 -0700

    Problem 3 done

--- problem3.cpp
+++ problem3.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 using namespace std;
 
+// Global variables
 int x, y;
 int *px, *py;
 
 int f() {
+    // S is the sum of the values of x and y
     int s = *px + *py;
+    // Print the value of S
     cout << s << endl;
+    // Return the value of S
     return s;
 }
 
 int main() {
+    // Assign value 2 to x and y
     x = y = 2;
+    // This is equivalent to:
+    // y = 2;
+    // x = y;
+
+    // Assign address of x to px and address of y to py
     px = &x; py = &y;
+    // This is equivalent to:
+    // px = &x;
+    // py = &y;
+
+    // Assign value of f() to x and y
     x = y = f();
+    // This is equivalent to:
+    // y = f();
+    // x = y;
+
+    // Call f() again
     cout << f() << endl;
 }
